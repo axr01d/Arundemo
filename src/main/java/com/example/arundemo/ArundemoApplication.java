1a0b7ea00ddbfd6938d6f0673e09c04ce1ecccf0
Arunkumar Ruppa Subramanian <arunkumar@sherwin.com>
2022-05-12 10:20:48 -0400

    Initial commit

---
+++ src/main/java/com/example/arundemo/ArundemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.arundemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArundemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArundemoApplication.class, args);
+	}
+
+}
